5e945d935d1365a874472e398a567c6d6716ec59
spacewalk00 <ju001217@gmail.com>
2023-01-15 20:08:02 +0900

    [ADD] 툴바, 메뉴바 공통 화면 추가

--- Blu-e/app/src/main/java/com/example/blu_e/MainActivity.kt
+++ Blu-e/app/src/main/java/com/example/blu_e/MainActivity.kt
@@ -6,9 +6,50 @@ import com.example.blu_e.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(viewBinding.containerFragment.id, HomeFragment())
+            .commitAllowingStateLoss()
+
+        viewBinding.navBottom.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_home -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(viewBinding.containerFragment.id, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_group -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(viewBinding.containerFragment.id, GroupFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_noti -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(viewBinding.containerFragment.id, NotiFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_mypage -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(viewBinding.containerFragment.id, MyPageFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_home
+        }
     }
+
+
 }

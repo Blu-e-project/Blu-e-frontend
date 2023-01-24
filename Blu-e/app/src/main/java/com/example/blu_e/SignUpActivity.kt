c5e18477a2d9fb5c16210f70afa24d5523e36ef5
mashin2002 <mashin2002@naver.com>
2023-01-24 22:43:23 +0900

    [ADD] 로그인 화면 생성

--- Blu-e/app/src/main/java/com/example/blu_e/SignUpActivity.kt
+++ Blu-e/app/src/main/java/com/example/blu_e/SignUpActivity.kt
@@ -2,14 +2,13 @@ package com.example.blu_e
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.blu_e.databinding.ActivityMainBinding
 import com.example.blu_e.databinding.ActivitySignUpBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var viewBinding:ActivitySignUpBinding
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-    }
+         }
 }

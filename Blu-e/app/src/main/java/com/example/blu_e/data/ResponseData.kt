9be3f9170d7e457c94114f05b8e642781fc8e60a
LeeJuYeong0809 <km0623@catholic.ac.kr>
2023-02-09 16:43:53 +0900

    [MOD] 비밀번호 수정 서버 연결

--- Blu-e/app/src/main/java/com/example/blu_e/data/ResponseData.kt
+++ Blu-e/app/src/main/java/com/example/blu_e/data/ResponseData.kt
@@ -1,3 +1,7 @@
 package com.example.blu_e.data
 
-data class ResponseData(val isSuccess: Boolean, val code: Int, val message: String)
+data class ResponseData(
+    val isSuccess: Boolean,
+    val code: Int,
+    val message: String
+    )

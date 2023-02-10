01bcba00c2d712ef7ced278c9afb8aafbbdcb226
kyumin <dkyuuum@gmail.com>
2023-02-10 12:12:29 +0900

    [CHORE] 메인화면 데이터 코드 연결 수정

--- Blu-e/app/src/main/java/com/example/blu_e/login/LoginActivity.kt
+++ Blu-e/app/src/main/java/com/example/blu_e/login/LoginActivity.kt
@@ -29,7 +29,7 @@ class LoginActivity : AppCompatActivity() {
         viewBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         var jwt: String?
-        
+
         //로그인 기능만 구현함 실패시 ui 수정 필요
         viewBinding.loginBtn.setOnClickListener {
             val id = viewBinding.userId.text.toString()
@@ -43,17 +43,17 @@ class LoginActivity : AppCompatActivity() {
 
                 override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                     val responseData = response.body()
-                        //성공하면
+                    //성공하면
                     if (responseData != null) {
                         if(responseData.code == 1000) {
                             Log.d("login", "성공")
                             jwt = responseData.result.jwt
                             Log.d("login", "${jwt}")
                             MainApplication.prefs.setString("blu-e-access-token", jwt!!)
-                           val userId = responseData.result.userId
+                            val userId = responseData.result.userId
                             MainApplication.prefs.setString("userId", userId.toString())
                             //멘티에 아이디가 있으면 꺼냄
-                            api.findMenteeID(jwt!!, userId).enqueue(object: Callback<FindMenteeIdResponse>{
+                            api.findMenteeID(userId).enqueue(object: Callback<FindMenteeIdResponse>{
                                 override fun onResponse(
                                     call: Call<FindMenteeIdResponse>,
                                     response: Response<FindMenteeIdResponse>
@@ -76,7 +76,7 @@ class LoginActivity : AppCompatActivity() {
 
                             })
 
-                            api.findMentorID(jwt!!, userId).enqueue(object: Callback<FindMentorIdResponse>{
+                            api.findMentorID(userId).enqueue(object: Callback<FindMentorIdResponse>{
                                 override fun onResponse(
                                     call: Call<FindMentorIdResponse>,
                                     response: Response<FindMentorIdResponse>
@@ -118,14 +118,14 @@ class LoginActivity : AppCompatActivity() {
                             viewBinding.userPw.backgroundTintList = ColorStateList.valueOf(Color.rgb(255,0,0))
                         }
                     }
-                        //에러 메세지 1초만 띄우기
-            Handler().postDelayed(Runnable {
-                viewBinding.pwMsg.text = ""
-                viewBinding.idMsg.text = ""
-                viewBinding.userId.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,107,206))
-                viewBinding.userPw.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,107,206))
-            }, 1000)
-                    }
+                    //에러 메세지 1초만 띄우기
+                    Handler().postDelayed(Runnable {
+                        viewBinding.pwMsg.text = ""
+                        viewBinding.idMsg.text = ""
+                        viewBinding.userId.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,107,206))
+                        viewBinding.userPw.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,107,206))
+                    }, 1000)
+                }
 
 
                 override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
